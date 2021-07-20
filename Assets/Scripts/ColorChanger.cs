ba098ebac510ac10b5882732c606d0f765e6c705
Mikhail Sintsev <mikhail.sintsev@gmail.com>
2021-07-20 12:44:01 +0400

    Upgrade Unity version (2020.3.14f1)

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class ColorChanger : MonoBehaviour
 {
     private static Color color;
-
     public static Color Color { get => color; private set => color = value; }
+
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.O))
-            Color = new Color(Random.value, Random.value, Random.value);
+        Color = new Color(Random.value, Random.value, Random.value);
     }
 }
